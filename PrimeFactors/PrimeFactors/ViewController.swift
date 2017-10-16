8e1c5f3253753ee6691a7948d9bc35ee4121e78f
primetimer <esjot@stephans-mbp.lan>
2017-10-16 21:01:42 +0200

    Initial

---
+++ PrimeFactors/PrimeFactors/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PrimeFactors
+//
+//  Created by Stephan Jancar on 16.10.17.
+//  Copyright © 2017 Stephan Jancar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
